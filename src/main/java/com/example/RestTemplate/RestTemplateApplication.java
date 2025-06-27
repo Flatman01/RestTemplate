8d4ecc25025d92726c6ca532fb7fc9e96e735956
Flatman01 <deeropro@yandex.ru>
2025-06-27 12:09:47 +0300

    cookie

--- src/main/java/com/example/RestTemplate/RestTemplateApplication.java
+++ src/main/java/com/example/RestTemplate/RestTemplateApplication.java
@@ -1,9 +1,13 @@
 package com.example.RestTemplate;
 
 import com.example.RestTemplate.model.User;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.GsonBuilderUtils;
@@ -26,11 +30,23 @@ public class RestTemplateApplication {
                 new ParameterizedTypeReference<List<User>>() {
                 }
         );
+        List<String> cookies = response.getHeaders().get(HttpHeaders.SET_COOKIE);
+        String sessionId = null;
+        if (cookies != null) {
+            for (String cookie : cookies) {
+                if (cookie.startsWith("JSESSIONID=")) {
+                    sessionId = cookie.split("=")[1];
+                    break;
+                }
+            }
+        }
+        System.out.println("Номер сессии: " + sessionId);
+        System.out.println("Cookie: " + cookies);
         List<User> users = response.getBody();
         if (users != null) {
             users.forEach(System.out::println);
         } else {
-            System.out.println("No users received.");
+            System.out.println("Пользователи не найдены ");
         }
     }
 }
