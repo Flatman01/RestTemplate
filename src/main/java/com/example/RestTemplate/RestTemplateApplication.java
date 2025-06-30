d7de7dc1db8e6b4b4833cd8bdc5ed86eea8089c7
Flatman01 <deeropro@yandex.ru>
2025-06-30 21:06:51 +0300

    1234

--- src/main/java/com/example/RestTemplate/RestTemplateApplication.java
+++ src/main/java/com/example/RestTemplate/RestTemplateApplication.java
@@ -1,16 +1,10 @@
 package com.example.RestTemplate;
 
 import com.example.RestTemplate.model.User;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -23,6 +17,8 @@ public class RestTemplateApplication {
 
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://94.198.50.185:7081/api/users";
+
+
         ResponseEntity<List<User>> response = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
@@ -44,9 +40,59 @@ public class RestTemplateApplication {
         System.out.println("Cookie: " + cookies);
         List<User> users = response.getBody();
         if (users != null) {
-            users.forEach(System.out::println);
-        } else {
-            System.out.println("Пользователи не найдены ");
+            for (User user : users) {
+                System.out.println(user);
+            }
         }
+
+        if (sessionId != null) {
+            User newUser = new User();
+            newUser.setId(3L);
+            newUser.setName("James");
+            newUser.setLastName("Brown");
+            newUser.setAge((byte) 24);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.add("Cookie", "JSESSIONID=" + sessionId);
+            HttpEntity<User> entity = new HttpEntity<>(newUser, headers);
+            ResponseEntity<String> postResponse = restTemplate.exchange(
+                    url,
+                    HttpMethod.POST,
+                    entity,
+                    String.class
+            );
+            System.out.println(postResponse.getBody());
+        }
+
+        User updateUser = new User();
+        updateUser.setId(3L);
+        updateUser.setName("Thomas");
+        updateUser.setLastName("Shelby");
+        updateUser.setAge((byte) 24);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Cookie", "JSESSIONID=" + sessionId);
+        HttpEntity<User> entity = new HttpEntity<>(updateUser, headers);
+        ResponseEntity<String> postResponse = restTemplate.exchange(
+                url,
+                HttpMethod.PUT,
+                entity,
+                String.class
+        );
+        System.out.println(postResponse.getBody());
+
+        HttpHeaders deleteUser = new HttpHeaders();
+        deleteUser.setContentType(MediaType.APPLICATION_JSON);
+        deleteUser.add("Cookie", "JSESSIONID=" + sessionId);
+        HttpEntity<String> deleteEntity = new HttpEntity<>(deleteUser);
+        ResponseEntity<String> deleteResponse = restTemplate.exchange(
+                url + "/3",
+                HttpMethod.DELETE,
+                deleteEntity,
+                String.class
+        );
+        System.out.println(deleteResponse.getBody());
     }
+
 }
